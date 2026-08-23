e7630557a972c4fdc383790dc10c234972209dca
malathi-2019 <maluvetri2005@gmail.com>
2026-08-23 22:34:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,34 @@ function toggleMenu() {
     const navLinks = document.getElementById("navLinks");
 
     navLinks.classList.toggle("show");
+const statCircles = document.querySelectorAll(".stat-circle");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            statCircles.forEach((circle, index) => {
+
+                setTimeout(() => {
+                    circle.classList.add("show");
+                }, index * 300);
+
+            });
+
+        }
+
+    });
+
+}, {
+    threshold: 0.3
+});
+
+
+const advocateSection = document.querySelector(".advocate-profile");
+
+if (advocateSection) {
+    observer.observe(advocateSection);
+}
 }
